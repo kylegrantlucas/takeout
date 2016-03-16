aff828d1887f6276fbdb104b040de899f449b51f
Kyle Lucas <kglucas93@gmail.com>
2016-03-16 15:45:18 -0700

    Allow for 100s

--- lib/takeout/client.rb
+++ lib/takeout/client.rb
@@ -1,3 +1,4 @@
+require 'takout/core/ext/nil_class'
 module Takeout
   class Client
     require 'curb'
@@ -32,7 +33,7 @@ module Takeout
     attr_accessor :endpoint_prefix
 
     # A constant specifying the kind of event callbacks and if they should or should not raise an error
-    CALLBACKS = {failure: true, missing: true, redirect: false, success: false}
+    CALLBACKS = {failure: true, missing: true}
 
     # The main client initialization method.
     # ==== Attributes
@@ -125,12 +126,12 @@ module Takeout
           curl.password = options[:password]
         end
 
-        CALLBACKS.each { |callback_type,failure| curl.send("on_#{callback_type}") {|response| @parsed_body, @failure = Takeout::Response.new(headers: parse_response_headers(response.head), body: Oj.load(response.body_str), response: response), failure} }
+        CALLBACKS.each { |callback_type,failure| curl.send("on_#{callback_type}") {|response| @failure = failure}}
       end
 
       raise Takeout::EndpointFailureError.new(curl, request_type, @parsed_body) if @failure
 
-      return @parsed_body
+      return Takeout::Response.new(headers: parse_response_headers(curl.head), body: Oj.load(curl.body_str), response: curl)
     end
 
 
@@ -178,7 +179,7 @@ module Takeout
     end
 
     def parse_response_headers(header_string)
-      header_string.split("\r\n")[1..-1].map {|x| {x.split(': ')[0].to_sym => x.split(': ')[1]} }.reduce({}, :update)
+      header_string.split("\r\n")[1..-1].map {|x| {x.split(': ')[0].to_sym => x.split(': ')[1]} }.reduce({}, :update) if header_string && !header_string.empty?
     end
 
     def method_missing(method_sym, *attributes, &block)
